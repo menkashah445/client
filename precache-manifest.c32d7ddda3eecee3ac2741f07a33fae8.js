e0047ab09a1299c55d7f308a5f2b612c82bf40ef
menkashah445 <menkashah060@gmail.com>
2024-03-18 12:28:10 +0530

    Updates

---
+++ precache-manifest.c32d7ddda3eecee3ac2741f07a33fae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfaa85c252a58cec1a8a912e0eef2d12",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "756ca6ec30547f38befa",
+    "url": "/client/static/css/main.d2535957.chunk.css"
+  },
+  {
+    "revision": "697f880b142ca6e24bf6",
+    "url": "/client/static/js/2.baf8cddd.chunk.js"
+  },
+  {
+    "revision": "9d0cc3cfc905af4a6940",
+    "url": "/client/static/js/3.69ba5426.chunk.js"
+  },
+  {
+    "revision": "756ca6ec30547f38befa",
+    "url": "/client/static/js/main.7ed63b19.chunk.js"
+  },
+  {
+    "revision": "4497b7bd3960b50934e8",
+    "url": "/client/static/js/runtime~main.52e88fdd.js"
+  }
+]);
